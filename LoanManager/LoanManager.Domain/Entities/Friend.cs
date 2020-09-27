44b064d3274dd6a8c0a960228b92ae665dcd70d0
bruno prado <brunomcp2010@gmail.com>
2020-09-27 01:34:18 -0300

    feat:created domain structure

---
+++ LoanManager/LoanManager.Domain/Entities/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Domain.Entities
+{
+    public class Friend
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ LoanManager/LoanManager.Domain/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LoanManager.Domain.Interfaces.Repositories
+{
+    public interface IRepository<TKey, T>
+    {
+        Task CreateAsync(T entity);
+        Task DeleteAsync(TKey id);
+        Task<T> ReadAsync(TKey id);
+        IEnumerable<T> ReadAll();
+        Task<IEnumerable<T>> ReadAllAsync();
+        void Update(T entity);
+        Task<int> SaveChangesAsync();
+    }
+}
