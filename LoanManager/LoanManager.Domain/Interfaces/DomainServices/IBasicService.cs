82a34d386e1bb7c410dfe2a8ab08b5e0613b3c66
bruno prado <brunomcp2010@gmail.com>
2020-09-27 14:33:40 -0300

    add contracts for domain services and base repositories

---
+++ LoanManager/LoanManager.Domain/Interfaces/DomainServices/IBasicService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManager.Domain.Interfaces.DomainServices
+{
+    public interface IBasicService<TKey, T>
+    {
+        Task CreateAsync(T entity);
+        Task<IEnumerable<T>> ReadAllAsync(int offset, int limit);
+        Task DeleteAsync(TKey id);
+    }
+}
