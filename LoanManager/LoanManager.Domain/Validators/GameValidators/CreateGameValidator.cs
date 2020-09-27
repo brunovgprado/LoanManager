b3d0056afdcac0d612288f16d2c7587368bcf87f
Bruno Prado <bruno.prado@squadra.com.br>
2020-09-27 17:15:21 -0300

    add domain validators and empty test project

---
+++ LoanManager/LoanManager.Domain/Validators/GameValidators/CreateGameValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using LoanManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Domain.Validators.GameValidators
+{
+    public class CreateGameValidator : AbstractValidator<Game>
+    {
+        public CreateGameValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .Must(x => !String.IsNullOrWhiteSpace(x))
+                .WithMessage("Game name can't be empty");
+
+            RuleFor(x => x.Platform)
+                .NotNull()
+                .Must(x => 
+                    !String.IsNullOrEmpty(x.Name) &&
+                    !String.IsNullOrWhiteSpace(x.Name)) 
+                .WithMessage("Platform name can't be empty");
+        }
+    }
+}
---
+++ LoanManager/LoanManager.Domain/Validators/LoanValidators/CreateLoanValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using LoanManager.Domain.Entities;
+using System;
+
+namespace LoanManager.Domain.Validators.LoanValidators
+{
+    public class CreateLoanValidator : AbstractValidator<Loan>
+    {
+        public CreateLoanValidator()
+        {
+            RuleFor(x => x.GameId)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Game id can't be empty");
+
+            RuleFor(x => x.FriendId)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Friend id can't be empty");
+
+        }
+    }
+}
