32a3df585c036b88daa183ff064b81d3736a9a45
bruno prado <brunomcp2010@gmail.com>
2020-09-27 12:36:07 -0300

    add Game value objects and README

--- LoanManager/LoanManager.Domain/Entities/Game.cs
+++ LoanManager/LoanManager.Domain/Entities/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LoanManager.Domain.ValueObjects.Game;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace LoanManager.Domain.Entities
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Genre { get; set; }
+        public Genre Genre { get; set; }
+        public Platform Platform { get; set; }
     }
 }
