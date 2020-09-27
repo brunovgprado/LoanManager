5f72833b99930b75ecf5752995ea836575f5d8bf
bruno prado <brunomcp2010@gmail.com>
2020-09-27 01:34:18 -0300

    feat:created domain structure

---
+++ LoanManager/LoanManager.Domain/Entities/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Domain.Entities
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ LoanManager/LoanManager.Domain/Entities/Loan.cs
@@ -0,0 +1,16 @@
+﻿using LoanManager.Domain.Enums.Loan;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Domain.Entities
+{
+    public class Loan
+    {
+        public Guid Id { get; set; }
+        public Friend Friend { get; set; }
+        public Game Game { get; set; }
+        public DateTime LoanDate { get; set; }
+        public Status Status { get; set; }
+    }
+}
