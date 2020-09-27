049caa1a88e2dd508bce9c9f020eeb3c310ce8d4
bruno prado <brunomcp2010@gmail.com>
2020-09-27 14:33:40 -0300

    add contracts for domain services and base repositories

---
+++ LoanManager/LoanManager.Domain/Interfaces/DomainServices/ILoanDomainService.cs
@@ -0,0 +1,16 @@
+﻿using LoanManager.Domain.Entities;
+using LoanManager.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManager.Domain.Interfaces.DomainServices
+{
+    public interface ILoanDomainService : IService<Guid, Loan>
+    {
+        void EndLoan(Guid id);
+        Task<Loan> ReadLoanByFriendNameAsync(string name);
+        Task<IEnumerable<Loan>> ReadLoanHistoryByGameNameAsync(int offset, int limit);
+    }
+}
--- LoanManager/LoanManager.Domain/Interfaces/Repositories/IRepository.cs
+++ LoanManager/LoanManager.Domain/Interfaces/Repositories/IRepository.cs
@@ -3,14 +3,9 @@ using System.Threading.Tasks;
 
 namespace LoanManager.Domain.Interfaces.Repositories
 {
-    public interface IRepository<TKey, T>
+    public interface IRepository<TKey, T> : IBasicRepository<TKey, T>
     {
-        Task CreateAsync(T entity);
-        Task DeleteAsync(TKey id);
         Task<T> ReadAsync(TKey id);
-        IEnumerable<T> ReadAll();
-        Task<IEnumerable<T>> ReadAllAsync();
         void Update(T entity);
-        Task<int> SaveChangesAsync();
     }
 }
