e118cdd31177910c52139af886e01ae73fe6be1f
Bruno Prado <bruno.prado@squadra.com.br>
2020-09-27 17:15:21 -0300

    add domain validators and empty test project

--- LoanManager/LoanManager.Domain/Entities/Loan.cs
+++ LoanManager/LoanManager.Domain/Entities/Loan.cs
@@ -8,9 +8,9 @@ namespace LoanManager.Domain.Entities
     public class Loan
     {
         public Guid Id { get; set; }
-        public Friend Friend { get; set; }
-        public Game Game { get; set; }
+        public Guid FriendId { get; set; }
+        public Guid GameId { get; set; }
         public DateTime LoanDate { get; set; }
-        public Status Status { get; set; }
+        public Status LoanStatus { get; set; }
     }
 }
---
+++ LoanManager/LoanManager.Domain/Validators/FriendValidators/CreateFriendValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using LoanManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Domain.Validators.FriendValidators
+{
+    public class CreateFriendValidator : AbstractValidator<Friend>
+    {
+        public CreateFriendValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Must(x => 
+                    !String.IsNullOrWhiteSpace(x))
+                .WithMessage("Friend name can't be empty");
+        }
+    }
+}
