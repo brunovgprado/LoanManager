ae77223446f13e4efed60841fafa7ea9a6406c52
Bruno Prado <bruno.prado@squadra.com.br>
2020-09-28 17:17:31 -0300

    feature: implemented UnitOfWork, GameRepository and DataAccessConfiguration

--- LoanManager/LoadManager.Application/DependencyInjection/ValidatorConfiguration.cs
+++ LoanManager/LoadManager.Application/DependencyInjection/ValidatorConfiguration.cs
@@ -3,15 +3,17 @@ using LoanManager.Domain.Validators.GameValidators;
 using LoanManager.Domain.Validators.LoanValidators;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace LoanManager.Api.Configurations
+namespace LoanManager.Application.DependencyInjection
 {
     public static class ValidatorConfiguration
     {
-        public static void ConfigureServices(IServiceCollection services)
+        public static IServiceCollection ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<CreateGameValidator, CreateGameValidator>();
             services.AddSingleton<CreateFriendValidator, CreateFriendValidator>();
             services.AddSingleton<CreateLoanValidator, CreateLoanValidator>();
+
+            return services;
         }
     }
 }
