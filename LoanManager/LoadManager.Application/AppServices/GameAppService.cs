be5c5dbfc95103f67b6e16d1780b233a972aa75c
Bruno Prado <bruno.prado@squadra.com.br>
2020-09-28 22:04:00 -0300

    add App services interfaces, GameAppService and DomainService, UnityOfWork and IoC project

---
+++ LoanManager/LoadManager.Application/AppServices/GameAppService.cs
@@ -0,0 +1,39 @@
+﻿using LoanManager.Application.Interfaces.AppServices;
+using LoanManager.Application.Models.DTO;
+using LoanManager.Domain.Interfaces.DomainServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Application.AppServices
+{
+    public class GameAppService : IGameAppService
+    {
+        private readonly IGameDomainService _gameDomainService;
+
+        public GameAppService(IGameDomainService gameDomainService)
+        {
+            _gameDomainService = gameDomainService;
+        }
+
+        public void Create(GameDto game)
+        {
+            _gameDomainService.CreateAsync(game);
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameDto Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<GameDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LoanManager/LoanManager.IoC/Shared/UtilsConfigurations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.IoC.Shared
+{
+    public class UtilsConfigurations
+    {
+    }
+}
