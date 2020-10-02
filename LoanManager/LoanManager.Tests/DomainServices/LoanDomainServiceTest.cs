ff6e4102dc381bb55df31b1e0797f17fee496163
Bruno Prado <brunomcp2010@gmail.com>
2020-10-01 23:49:47 -0300

    changed validations class to domain layer

---
+++ LoanManager/LoanManager.Tests/DomainServices/LoanDomainServiceTest.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using LoanManager.Domain.DomainServices;
+using LoanManager.Domain.Interfaces;
+using LoanManager.Domain.Interfaces.DomainServices;
+using LoanManager.Domain.Interfaces.Repositories;
+using LoanManager.Domain.Validators.LoanValidators;
+using LoanManager.Infrastructure.DataAccess.Repositories;
+using LoanManager.Tests.Builders;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LoanManager.Tests.DomainServices
+{
+    public class LoanDomainServiceTest
+    {
+        private readonly CreateLoanValidator _createLoanValidator;
+        private readonly Mock<IGameRepository> _gameRepositoryMock;
+        private readonly Mock<IFriendRepository> _friendRepositoryMock;
+        private readonly Mock<ILoanRepository> _loanRepositoryMock;
+        private readonly IUnitOfWork _unityOfWork;
+        private readonly ILoanDomainService _loanDomainService;
+
+        public LoanDomainServiceTest()
+        {
+            _createLoanValidator = new CreateLoanValidator();
+            _gameRepositoryMock = new Mock<IGameRepository>();
+            _friendRepositoryMock = new Mock<IFriendRepository>();
+            _loanRepositoryMock = new Mock<ILoanRepository>();
+
+            _unityOfWork = new UnitOfWork(
+                _gameRepositoryMock.Object,
+                _friendRepositoryMock.Object,
+                _loanRepositoryMock.Object);
+
+            _loanDomainService = new LoanDomainService(
+                    _unityOfWork,
+                    _createLoanValidator);
+        }
+
+        [Fact]
+        public async Task CreateGame_Success()
+        {
+            //Arrange
+            var entity = new LoanBuilder().Build();
+
+            //Act
+            var response = await _loanDomainService.CreateAsync(entity);
+
+            //Assert
+            response.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateGame_With_GameId_Empty()
+        {
+            //Arrange
+            var entity = new LoanBuilder().WithGameIdEmpty();
+
+            //Act/Assert
+            await Assert.ThrowsAsync<ValidationException>(async
+                () => await _loanDomainService.CreateAsync(entity));
+        }
+
+        [Fact]
+        public async Task CreateGame_With_FriendId_Empty()
+        {
+            //Arrange
+            var entity = new LoanBuilder().WithFriendIdEmpty();
+
+            //Act/Assert
+            await Assert.ThrowsAsync<ValidationException>(async
+                () => await _loanDomainService.CreateAsync(entity));
+        }
+    }
+
+
+}
---
+++ LoanManager/LoanManager.Tests/Utils/FakerPtbr.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoanManager.Tests.Utils
+{
+    public class FakerPtbr
+    {
+        public static Faker CreateFaker()
+        {
+            return new Faker("pt_BR");
+        }
+    }
+}
