9b861e362c5d9dcfe557bf380f6a20d5b54ae436
Bruno Prado <bruno.prado@sitemercado.com.br>
2022-10-15 01:24:03 -0300

    fix: fix code smell identified by sonarqube

--- src/LoanManager.Domain/Interfaces/Repositories/IBaseRepository.cs
+++ src/LoanManager.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -6,9 +6,9 @@ namespace LoanManager.Domain.Interfaces.Repositories
     public interface IBaseRepository<in TKey, T>
     {
         Task<bool> CreateAsync(T entity);
+        Task<T> GetAsync(TKey id);
         Task<IEnumerable<T>> GetAsync(int offset, int limit);
         Task<bool> DeleteAsync(TKey id);
         Task<bool> UpdateAsync(T entity);
-        Task<T> GetAsync(TKey id);
     }
 }
