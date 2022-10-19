9481c0879c6d3d0a2950900e8e684c6abd6c4ff3
= <brunomcp2010@gmail.com>
2022-10-19 09:44:26 -0300

    refactor: add notification handler middleware

---
+++ src/LoanManager.Infrastructure.CrossCutting/Contracts/IApplicationInsights.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using System;
+using System.Collections.Generic;
+
+namespace LoanManager.Infrastructure.CrossCutting.Contracts
+{
+    public interface IApplicationInsights
+    {
+        void TrackEvent(EventTelemetry telemetry);
+        void TrackException(Exception exception);
+        void TrackException(Exception exception, IDictionary<string, string> properties);
+    }
+}
